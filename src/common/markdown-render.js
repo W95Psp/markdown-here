ad766200219f571f23ee9385c5f59704e13d4938
Lucas Franceschino <lucas.franceschino@inria.fr>
2019-08-14 13:54:00 +0200

    Quick & dirty fix for getting base64 images out of google formulas

--- src/common/markdown-render.js
+++ src/common/markdown-render.js
@@ -15,105 +15,129 @@
 
 ;(function() {
 
-"use strict";
-/*global module:false*/
+    "use strict";
+    /*global module:false*/
 
-var MarkdownRender = {};
+    var MarkdownRender = {};
+
+    const toDataURL = url => fetch(url)
+	  .then(response => response.blob())
+	  .then(blob => new Promise((resolve, reject) => {
+	      const reader = new FileReader();
+	      reader.onloadend = () => resolve(reader.result);
+	      reader.onerror = reject;
+	      reader.readAsDataURL(blob);
+	  }));
 
 
-/**
- Using the functionality provided by the functions htmlToText and markdownToHtml,
- render html into pretty text.
- */
-function markdownRender(mdText, userprefs, marked, hljs) {
-  function mathify(mathcode) {
-    return userprefs['math-value']
-            .replace(/\{mathcode\}/ig, mathcode)
-            .replace(/\{urlmathcode\}/ig, encodeURIComponent(mathcode));
-  }
+    /**
+       Using the functionality provided by the functions htmlToText and markdownToHtml,
+       render html into pretty text.
+    */
+    async function markdownRender(mdText, userprefs, marked, hljs) {
+	let mathifyCache = [];
+	let mathifyCachePlaceholderFor = id => "TO_BE_CHANGED_TO_MATH_"+(+id)+"_";
+	function mathify(mathcode) {
+	    mathifyCache.push(mathcode);
+	    return mathifyCachePlaceholderFor(mathifyCache.length - 1);
+	    // return userprefs['math-value']
+	    // 	.replace(/\{mathcode\}/ig, mathcode)
+	    // 	.replace(/\{urlmathcode\}/ig, encodeURIComponent(mathcode));
+	}
 
-  // Hook into some of Marked's renderer customizations
-  var markedRenderer = new marked.Renderer();
+	// Hook into some of Marked's renderer customizations
+	var markedRenderer = new marked.Renderer();
 
-  var sanitizeLinkForAnchor = function(text) {
-    return text.toLowerCase().replace(/[^\w]+/g, '-');
-  };
+	var sanitizeLinkForAnchor = function(text) {
+	    return text.toLowerCase().replace(/[^\w]+/g, '-');
+	};
 
-  var defaultHeadingRenderer = markedRenderer.heading;
-  markedRenderer.heading = function (text, level, raw) {
-    if (userprefs['header-anchors-enabled']) {
-      // Add an anchor right above the heading. See MDH issue #93.
-      var sanitizedText = sanitizeLinkForAnchor(text);
-      var anchorLink = '<a href="#" name="' + sanitizedText + '"></a>';
-      return '<h' + level + '>' +
-             anchorLink +
-             text +
-             '</h' + level + '>\n';
+	var defaultHeadingRenderer = markedRenderer.heading;
+	markedRenderer.heading = function (text, level, raw) {
+	    if (userprefs['header-anchors-enabled']) {
+		// Add an anchor right above the heading. See MDH issue #93.
+		var sanitizedText = sanitizeLinkForAnchor(text);
+		var anchorLink = '<a href="#" name="' + sanitizedText + '"></a>';
+		return '<h' + level + '>' +
+		    anchorLink +
+		    text +
+		    '</h' + level + '>\n';
+	    }
+	    else {
+		return defaultHeadingRenderer.call(this, text, level, raw);
+	    }
+	};
+
+	var defaultLinkRenderer = markedRenderer.link;
+	markedRenderer.link = function(href, title, text) {
+	    // Added to fix MDH issue #57: MD links should automatically add scheme.
+	    // Note that the presence of a ':' is used to indicate a scheme, so port
+	    // numbers will defeat this.
+	    href = href.replace(/^(?!#)([^:]+)$/, 'http://$1');
+
+	    if (userprefs['header-anchors-enabled']) {
+		// Add an anchor right above the heading. See MDH issue #93.
+		if (href.indexOf('#') === 0) {
+		    href = '#' + sanitizeLinkForAnchor(href.slice(1).toLowerCase());
+		}
+	    }
+
+	    return defaultLinkRenderer.call(this, href, title, text);
+	};
+
+	var markedOptions = {
+	    renderer: markedRenderer,
+	    gfm: true,
+	    pedantic: false,
+	    sanitize: false,
+	    tables: true,
+	    smartLists: true,
+	    breaks: userprefs['gfm-line-breaks-enabled'],
+	    smartypants: true,
+	    // Bit of a hack: highlight.js uses a `hljs` class to style the code block,
+	    // so we'll add it by sneaking it into this config field.
+	    langPrefix: 'hljs language-',
+	    math: userprefs['math-enabled'] ? mathify : null,
+	    highlight: function(codeText, codeLanguage) {
+		if (codeLanguage &&
+		    hljs.getLanguage(codeLanguage.toLowerCase())) {
+		    return hljs.highlight(codeLanguage.toLowerCase(), codeText).value;
+		}
+
+		return codeText;
+	    }
+	};
+
+	var renderedMarkdown = marked(mdText, markedOptions);
+	for(let i in mathifyCache){
+	    let mathcode = mathifyCache[i];
+	    
+	    let html = userprefs['math-value']
+	    	.replace(/\{mathcode\}/ig, mathcode)
+	    	.replace(/\{urlmathcode\}/ig, encodeURIComponent(mathcode));
+	    let url = html.match(/src="([^"]*)"/)[1];
+	    let base64 = await toDataURL(url);
+	    html = html.replace(url, base64);
+	    renderedMarkdown = renderedMarkdown.replace(mathifyCachePlaceholderFor(i), html);
+	}
+
+	return renderedMarkdown;
     }
-    else {
-      return defaultHeadingRenderer.call(this, text, level, raw);
+
+
+    // Expose these functions
+
+    MarkdownRender.markdownRender = markdownRender;
+
+    var EXPORTED_SYMBOLS = ['MarkdownRender'];
+
+    if (typeof module !== 'undefined') {
+	module.exports = MarkdownRender;
+    } else {
+	this.MarkdownRender = MarkdownRender;
+	this.EXPORTED_SYMBOLS = EXPORTED_SYMBOLS;
     }
-  };
-
-  var defaultLinkRenderer = markedRenderer.link;
-  markedRenderer.link = function(href, title, text) {
-    // Added to fix MDH issue #57: MD links should automatically add scheme.
-    // Note that the presence of a ':' is used to indicate a scheme, so port
-    // numbers will defeat this.
-    href = href.replace(/^(?!#)([^:]+)$/, 'http://$1');
-
-    if (userprefs['header-anchors-enabled']) {
-      // Add an anchor right above the heading. See MDH issue #93.
-      if (href.indexOf('#') === 0) {
-        href = '#' + sanitizeLinkForAnchor(href.slice(1).toLowerCase());
-      }
-    }
-
-    return defaultLinkRenderer.call(this, href, title, text);
-  };
-
-  var markedOptions = {
-    renderer: markedRenderer,
-    gfm: true,
-    pedantic: false,
-    sanitize: false,
-    tables: true,
-    smartLists: true,
-    breaks: userprefs['gfm-line-breaks-enabled'],
-    smartypants: true,
-    // Bit of a hack: highlight.js uses a `hljs` class to style the code block,
-    // so we'll add it by sneaking it into this config field.
-    langPrefix: 'hljs language-',
-    math: userprefs['math-enabled'] ? mathify : null,
-    highlight: function(codeText, codeLanguage) {
-        if (codeLanguage &&
-            hljs.getLanguage(codeLanguage.toLowerCase())) {
-          return hljs.highlight(codeLanguage.toLowerCase(), codeText).value;
-        }
-
-        return codeText;
-      }
-    };
-
-  var renderedMarkdown = marked(mdText, markedOptions);
-
-  return renderedMarkdown;
-}
-
-
-// Expose these functions
-
-MarkdownRender.markdownRender = markdownRender;
-
-var EXPORTED_SYMBOLS = ['MarkdownRender'];
-
-if (typeof module !== 'undefined') {
-  module.exports = MarkdownRender;
-} else {
-  this.MarkdownRender = MarkdownRender;
-  this.EXPORTED_SYMBOLS = EXPORTED_SYMBOLS;
-}
 
 }).call(function() {
-  return this || (typeof window !== 'undefined' ? window : global);
+    return this || (typeof window !== 'undefined' ? window : global);
 }());
